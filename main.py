8546b786b71c44b0a8f97dde3b74b9d1d8eea92b
de-dust-enjoyer <simonhartig@gmail.com>
2024-03-26 22:38:41 +0100

    moved Projectile-class to main.py and deleted projectile.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from math import atan, degrees
 # custom modules:
 import unit_info, turret_info, projectile_info
 from spritesheet import SpriteSheet
-from particle import Particle
 
 # initialize pygame module
 pygame.init()
@@ -2450,6 +2449,20 @@ class Blood:
 
 	def update(self):
 		self.move()
+
+
+class Particle:
+	def __init__(self, color:tuple, size:tuple, starting_pos:tuple, lifetime:int, type:str, camera_offset_x:int = 0, alpha:int = 255):
+		self.color = (color[0], color[1], color[2], alpha)
+		self.width = size[0]
+		self.height = size[1]
+		self.starting_pos_x = starting_pos[0]
+		self.starting_pos_y = starting_pos[1]
+		self.lifetime = lifetime * 60
+		self.type = type
+		self.starting_camera_offset = camera_offset_x
+		self.rect = pygame.Rect(self.starting_pos_x, self.starting_pos_y, self.width, self.height)
+		
 
 
 
