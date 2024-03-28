b90be6db05eeb55e181ef61c5e1da64cbe3d76f7
de-dust-enjoyer <simonhartig@gmail.com>
2024-03-28 12:56:33 +0100

    reset some variables

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class Game:
 
 		# time variables to keep track of time
 		self.frames_passed = 0
-		self.seconds_passed = 30
-		self.minutes_passed = 14
+		self.seconds_passed = 0
+		self.minutes_passed = 0
 
 		# chooses if window should be fullscreen (work in progress: window scaling does not work propperly)
 		if self.fullscreen:
